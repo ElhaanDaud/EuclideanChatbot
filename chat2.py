2ae45e09bdd7eccd826f8efc529dfb79b14cf266
smazmi <sadiquemobaraka5@gmail.com>
2024-04-03 01:07:32 +0530

    lol env safety

--- chat2.py
+++ chat2.py
@@ -5,7 +5,9 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain_community.chat_models import ChatOpenAI
+from dotenv import load_dotenv
 import time
+import os
 
 progress_bar = st.progress(0)
 for i in range(100):
@@ -13,7 +15,7 @@ for i in range(100):
     time.sleep(0.1)
 
 
-OPENAI_API_KEY=""   
+OPENAI_API_KEY=os.getenv("OPENAI_API_KEY")
 
 #Header of the webpage
 st.header("Euclidean Chatbot Project")
